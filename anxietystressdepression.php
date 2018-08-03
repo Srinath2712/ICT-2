68b2aee4f4ea4cda8f01c5fd8a3bc75a4085f074
nekkaraganti <31032982+nekkaraganti@users.noreply.github.com>
2018-08-03 13:47:16 +1000

    Update anxietystressdepression.php

--- anxietystressdepression.php
+++ anxietystressdepression.php
@@ -32,23 +32,70 @@
 
 
 
-  <?php include("include/all_main_head.inc") ?>
+  <?php include("include/index_main.inc") ?>
 
-<br><br><br><br><br><br>
+<br>
 
 <div class="container">
 
-  <h1>Anxiety content</h1>
+  <h1>Anxiety and Stress</h1>
+  
+  <p>We all experience stress in our lives but sometimes it becomes too much, and we can begin to feel anxious and worried about the smallest of things to the point where it begins to affect our daily functioning. Hypnotherapy works with the subconscious mind to get to the causes of those behaviours enabling you to let go and relax again.</p>
+  
+  <h4>What is Anxiety?</h4>
+  
+  <p>Anxiety is an uncontrollable and often irrational worrying about situations, events or everyday events that can be out of proportion to the situation.  You may find yourself anticipating disaster or catastrophising around situations that a part of you knows is really ok.  Perhaps you are finding it difficult to stop worrying about finances, exams or tests, job interviews, being on time, health issues or family problems to the point where it is beginning to interfere with your life.</p>
+  
+  <h4>What does Anxiety feel like?</h4>
+  
+  <p>Anxiety is an emotional reaction to our thoughts and feelings.  Situations do not automatically create anxiety, rather it is our reaction to them or how we think about them.  This emotional reaction, in turn, creates a body response producing a variety of physical symptoms including fatigue, insomnia, irritability, sweating, a sense of panic or agitation, difficulties breathing, muscle aches or difficulty swallowing.  </p>
+  
+  <h4>The Effects of Anxiety</h4>
+  
+  <p>Most people have been anxious at some time in their life and experienced these symptoms and other symptoms. Anxiety can be a useful tool in recognising a stress in our lives.  However, problems arise when the anxiety continues or comes back in a stronger form each time it is experienced.  Continued or chronic anxiety may lead to such conditions as irritable bowel syndrome, tension headaches or migraine, compulsive or obsessive behaviours, social anxiety and many more. It can be hard, at times, to pin point where this anxiety is coming from or, once located, hard to know how to stop it.  His is where hypnotherapy, a combination of counselling and hypnotherapy, can help. </p>
+  
+  <h4>How Hypnotherapy can help</h4>
+  
+  <p>Hypnotherapy has been shown to be effective in the treatment of anxiety by inducing a calming and relaxing state of mind and body, enabling the body to counteract the effects of increased levels of adrenalin in the system. Hypnotherapy goes further than just pleasant relaxation.  By exploring the root causes of the anxiety and dealing with any emergent concerns, anxiety and the resultant stress can be reduced.</p>
+  
+  <h4>STRESS</h4>
+  
+  <p>Stress is something we have all experienced.  A certain level of stress is good as it can move us into action and/or create the impetus to bring change into our lives.  Too much stress, however, can have negative effects on our bodies and emotional health, leading to a variety of illness or general malaise.  There are many things we can do to reduce stress but sometimes we can be too stressed to do the things we know we need to do.  In these situations, hypnotherapy can be a useful tool. Hypnotherapy provides opportunities to let go of stress and the anxiety around it. Through discussion you are able to get to the root cause of this stress and manage it with hypnosis, creating a release.</p>
+  
+  <p>In my sessions you will be given exercises to manage both stress and anxiety, enabling you to recreate the sense of peace and calm when you need to and, in the moment, and therefore change those established stress patterns to patterns of calm and ease. </p>  
+  
+  
+  <h1>Depression</h1>
+  
+  <h4>How do you know if you are depressed?</h4>
+  
+  <ul>
+  <li>Do you feel miserable?</li>
+  <li>Do you find yourself sitting, just thinking and not moving?</li>
+  <li>Has your appetite changed?</li>
+  <li>Are you having trouble sleeping?</li>
+  <li>Is it hard to make plans for the future?</li>
+  <li>Are you turning down social engagements to stay at home?</li>
+  <li>Do you spend a lot of time by yourself and are unhappy about it?</li>
+  <li>Are you having trouble in building relationships?</li>
+  <li>Are you emotional and feel like crying at others a lot?</li>
+  <li>Do you feel flat and empty?  </li>
+  <li>Do you feel there's no point?</li>
+  <li>Are you valued by others?</li>
+  </ul>
+  <p>Just think about these statements.  Because you have answered yes to some of them may not be indicative of depression.  Everyone feels these things from time to time.  If you find that these feelings persist, it may be that you are depressed.  The good news is there is something you can do about it.</p>
 
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-  <p>In tibique oportere eos. Ius phaedrum tractatos ne, dolore nonumy adipisci mea id. Ad porro cetero timeam vix, ex his laudem virtute, lorem voluptua salutatus ne vis. Ei oratio evertitur percipitur his, at aliquip meliore assueverit est. Ea possim lobortis constituto quo. Eam habeo vocent invidunt ei, te cum utinam splendide.</p>
+<h4>How can Hypnotherapy help?</h4>
 
-  <p>Probo omnium interpretaris ut duo, sit an agam numquam constituto. Ne mucius timeam laoreet pri, euismod impedit ius te. No per ponderum dissentiet, et saepe necessitatibus per. Graeci causae id vix, ex legimus minimum deleniti est, ad persecuti suscipiantur pro. An populo eripuit quo.</p>
 
-  <p>Ius cu nobis maluisset comprehensam, usu idque fugit ne, an est summo forensibus adversarium. Velit alterum gubergren per ut, propriae interesset ei eam. Solum ipsum ei usu. Qui an perfecto torquatos, ad sed quod epicuri. Esse facilisis mea eu.</p>
+  <p>Hypnotherapy can work well with depression.  Through discussion the underlying causes to your depression are uncovered and, through hypnosis, you are supported to let go of those events or situations which led to the depression.  </p>
 
-  <p>Ut mediocrem scriptorem neglegentur mei, ei vis justo mundi corpora. Vim ut meis suscipiantur, ex persius discere perpetua est. Ad labitur referrentur mel, ea erroribus abhorreant cum. Pri mentitum reprehendunt et, ei nam iisque albucius efficiendi.</p>
+  <p>Sometimes depression can become a habit - a way of being.  You know there's a happier, livelier and more joyful person wanting to show itself to the world but sometimes it's difficult to know how.  Hypnotherapy and hypnosis works to change those habitual pathways, leading you into the life you've always wanted.  </p>
+
+  <p>A huge amount of energy is tied up in depression.  Once released from this you will find yourself re-energised, motivated and planning for the future.  </p>
+
+  <p>If you have any concerns or questions about depression, please call me for a obligation free and confidential discussion.</p>
 
 </div>
 
