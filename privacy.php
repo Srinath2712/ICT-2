d2af93c81975f5287c37f65ae5bc4b0424d23a65
nekkaraganti <31032982+nekkaraganti@users.noreply.github.com>
2018-09-28 12:15:59 +1000

    Add files via upload

---
+++ privacy.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once 'connect.php';
+
+?>
+
+
+
+<!DOCTYPE html>
+
+
+
+<html lang="en">
+
+
+
+<head>
+
+    
+
+    <?php include("include/head.inc") ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+</head>
+
+
+
+<body>
+
+      <?php include("include/index_main.inc") ?>
+
+    <br>
+
+    <div class="container">
+
+
+
+  <h1>Privacy Policy</h1>
+
+  <P>
+ Maggie Maylin Clinical Hypnotherapy and Counselling is committed to protecting your own privacy online. You are free to visit our website, without the disclosure of your personal information. In circumstances, whereby we may discuss and advise you on the right products or services, we may ask you for personal information so that we can make the best recommendations. Any personal information you provide to us via this website is done so by you on a voluntary basis.
+Any information we gather that is related to general website usage, page views, and browsing patterns are collected at random and completely anonymously, without the disclosure of your personal data. We use this anonymous intelligence and data to continually refine the website content and provide a more seamless experience for our visitors. The Maggie Maylin Clinical Hypnotherapy and Counselling website contains a series of hypertext links to a number of external websites and resources. We hold no control and therefore accept no liability for the content or the availability of these websites.
+Maggie Maylin Clinical Hypnotherapy and Counselling is not responsible for the privacy practices of and of the external websites we link to. We provide these links for convenience, value, and reference only. We accept no liability for any of the information or resources they contain. We reserve the right to alter this privacy policy at any time and without prior notice. If you have any questions about our privacy policy, statements or processes, please do not hesitate to contact us.
+Whilst Maggie Maylin Clinical Hypnotherapy and Counselling and all the information contained in this website aims to provide you with the most up-to-date and complete information as possible, we cannot provide any guarantees with regards to how appropriate, consistent or complete the advice is for a specific of circumstances.
+The responsibility of interpreting the information on this website is yours. We take no responsibility for any subsequent action taken and therefore clearly state that Maggie Maylin Clinical Hypnotherapy and Counselling is not in any way or to any extent, responsible or accountable for any claim, loss or damage as a result of reading the content of this website. Whilst we may name specific brands, merchandise, and services on this website, Maggie Maylin Clinical Hypnotherapy and Counselling does not endorse or recommend any brands, merchandise or services – unless stated otherwise. We reserve the right to update, replace, modify or delete any information on this website without prior notice and we are under no obligation to update out-of-date information.
+The Maggie Maylin Clinical Hypnotherapy and Counselling company branding, logo design, service names, design marks, and slogans are the trademarks or service marks of Maggie Maylin Clinical Hypnotherapy and Counselling. This policy forbids the unauthorized use of any of the listed components in any advertisement, publicity or for any other marketing or commercial purposes. Written consent is required in all circumstances.
+
+
+      </p>
+
+
+
+
+
+
+
+
+
+    
+
+<!--footer section-->
+
+
+
+<?php include("include/footer.inc") ?>
+
+
+
+<script src="js/jquery.admin.js"></script>
+
+
+
+  <script src="js/jquery.min.js" ></script>
+
+
+
+  <script src="js/popper.min.js"></script>
+
+
+
+  <script src="js/bootstrap.min.js"></script>
+
+
+
+  <script src="js/navbar-fixed.js"></script>
+
+
+
+  <script src="js/slick.js"></script>
+
+
+
+  <script src="js/main.js"></script>
+
+
+
+  </body>
+
+
+
+  </html>
+
