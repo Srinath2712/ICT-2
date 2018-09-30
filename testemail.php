132b1880c205e63f090996e8e22c7c9ce59afed7
Srinath2712 <31033039+Srinath2712@users.noreply.github.com>
2018-09-30 17:53:38 +1000

    Testing

---
+++ testemail.php
@@ -0,0 +1,38 @@
+<!-- Author Srinath 
+
+code for preventing sql injection through email 
+
+-->
+
+
+<?php
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
